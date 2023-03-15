cf7bd39bd74d13a35f51e76d44579266e4ff0405
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-03-15 16:28:06 +0100

    logic

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,29 +1,46 @@
-import express,{Request, response, Response} from 'express'
+import express, { Request, response, Response } from "express";
+import { DataModel } from "../model/index";
 
 // export const getDAta = (req: Request, res: Response)=>{
 //     res.status(200).json("successful")
 // }
 
-export const createData = (req: Request, res: Response)=>{
-    const { phoneNumber, text, sessionId } = req.body
-    // console.log(phoneNumber);
-    
-    let response = ""
-    if(text === ''){
-        response = `CON\n enter your Name here`
+export const createData = (req: Request, res: Response) => {
+  const { phoneNumber, text, sessionId } = req.body;
+
+  let response = "";
+  if (text === "") {
+    response = `CON Enter your name here`;
+  }
+  if (text !== "") {
+    let array = text.split("*");
+    if (array.length > 1) {
+      response = `CON Enter your id number`;
+
+    } else if (array.length === 2) {
+      if (parseInt(array[1]) > 0) {
+        response = `CON Please confirm if you want to save the data. \n 1. Confirm \n2. Cancel`;
+        // response = `END Your name is ${array[0]} \n Your id_number is ${array[1]}`;
+      } else if (parseInt(array[2]) === 1) {
+        let data = new DataModel();
+        data.fullName = array[0];
+        data.id_number = array[1];
+        data.save(() => {
+          response = "END Your data has been saved successfully";
+        });
+      } else if (parseInt(array[2]) === 2) {
+        response = `END Sorry, data was not saved.`;
+      } else {
+        response = `END Network error, Please try again.`;
+      }
+    } else {
+      response = `END Network error, Please try again.`;
     }
+  }
 
-
-
-
-
-    setTimeout(()=>{
-        console.log(response, "2");
-        res.send(response)
-        res.end()
-    }, 2000)
-    
-    // res.set("Content-Type: text/plain");
-    // res.send(response);
-    
-}
+  setTimeout(() => {
+    console.log(response, "2");
+    res.send(response);
+    res.end();
+  }, 2000);
+};
